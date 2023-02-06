2f22c2416285d62fecdc64d81037393109cee7b2
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-02-06 08:44:04 -0300

    Creating project and git repository

---
+++ app/src/db/DbException.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package db;
+
+/**
+ *
+ * @author fabio
+ */
+public class DbException extends RuntimeException {
+//    private static final long serialVersionID = 1L;
+
+    public DbException(String msg) {
+        super(msg);
+    }
+    
+    
+}
