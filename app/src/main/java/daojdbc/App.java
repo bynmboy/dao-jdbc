ee801384cb8e8c6d903505642fd79e87d0a5f618
Fabio Carneiro <fabioaacarneiro@gmail.com>
2023-02-07 09:19:22 -0300

    delete implementation

--- app/src/main/java/daojdbc/App.java
+++ app/src/main/java/daojdbc/App.java
@@ -37,10 +37,20 @@ public class App {
         for (Seller obj : list) {
             System.out.println(obj);
         }
+//        
+//        System.out.println("\n=== Test 4: seller insert ===");
+//        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+//        sellerDao.insert(newSeller);
+//        System.out.println("Inserted! New id = " + newSeller.getId());
         
-        System.out.println("\n=== Test 4: seller insert ===");
-        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
-        sellerDao.insert(newSeller);
-        System.out.println("Inserted! New id = " + newSeller.getId());
+//        System.out.println("\n=== Test 5: seller update ===");
+//        seller = sellerDao.findById(1);
+//        seller.setName("Marta Wayne");
+//        sellerDao.update(seller);
+//        System.out.println("Updated complete!");
+        
+        System.out.println("\n=== Test 6: seller deleteById ===");
+        sellerDao.deleteById(100);
+        System.out.println("Delete complete!");
     }
 }
--- app/src/main/java/model/dao/impl/SellerDaoJDBC.java
+++ app/src/main/java/model/dao/impl/SellerDaoJDBC.java
@@ -71,12 +71,54 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller obj) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        PreparedStatement st = null;
+        
+        try {
+            st = conn.prepareStatement(
+                "update seller " +
+                "set Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? " +
+                "where Id = ? "
+            );
+            
+            st.setString(1, obj.getName());
+            st.setString(2, obj.getEmail());
+            st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+            st.setDouble(4, obj.getBaseSalary());
+            st.setInt(5, obj.getDepartment().getId());
+            st.setInt(6, obj.getId());
+            
+            st.executeUpdate();
+           
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(st);
+        }
     }
-
+    
     @Override
     public void deleteById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        PreparedStatement st = null;
+        
+        try {
+            st = conn.prepareStatement(
+                "delete from seller " +
+                "where Id = ? "
+            );
+            
+            st.setInt(1, id);
+            
+            int rowsAffected = st.executeUpdate();
+            
+            if (rowsAffected == 0) {
+                throw new DbException("The Seller does not exist on Database!");
+            }
+            
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(st);
+        }
     }
 
     @Override
